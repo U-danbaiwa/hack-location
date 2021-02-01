63ad7ccfc1a773bdb290b3019559ed0e020774b3
DANBAIWA HACKER <78322454+U-danbaiwa@users.noreply.github.com>
2021-02-01 11:43:59 +0100

    Update locate.py

--- locate.py
+++ locate.py
@@ -1,10 +1,10 @@
 import requests 
 import json 
 # Your API key, available from your account page 
-YOUR_GEOLOCATION_KEY=input('ENTER VICTIM IP')
+YOUR_GEOLOCATION_KEY=input('ENTER VICTIM IP: ')
 # URL to send the request to 
 request_url = 'https://ipgeolocation.abstractapi.com/v1/?api_key=' + YOUR_GEOLOCATION_KEY 
 response = requests.get(request_url)
 result = json.loads(response.content) 
 print(result) 
-print('ip_address: {}'.format(result['ip_address']))
+#print('ip_address: {}'.format(result['ip_address']))
