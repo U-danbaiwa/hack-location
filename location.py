1c2b9cf55f0871439f58553f0eddadcb713dd236
DANBAIWA HACKER <78322454+U-danbaiwa@users.noreply.github.com>
2021-02-01 19:07:20 +0100

    Update location.py

--- location.py
+++ location.py
@@ -1,32 +1,7 @@
-import argparse
-import requests, json
-import sys
-from sys import argv
-import os
-ip = input("enter your ip: ")
-
-api = "http://ip-api.com/json/"
-
-try:
-        data = requests.get(api+ip).json()
-        sys.stdout.flush()
-        a = lgreen+bold+"[$]"
-        b = cyan+bold+"[$]"
-        print (a, "[Victim]:", data['query'])
-        print(red+"<--------------->"+red)
-        print (b, "[ISP]:", data['isp'])
-        print(red+"<--------------->"+red)
-        print (a, "[Organisation]:", data['org'])
-        print(red+"<--------------->"+red)
-        print (b, "[City]:", data['city'])
-        print(red+"<--------------->"+red)
-        print (a, "[Region]:", data['region'])
-        print(red+"<--------------->"+red)
-        print (b, "[Longitude]:", data['lon'])
-        print(red+"<--------------->"+red)
-        print (a, "[Latitude]:", data['lat'])
-        print(red+"<--------------->"+red)
-        print (b, "[Time zone]:", data['timezone'])
-        print(red+"<--------------->"+red)
-        print (a, "[Zip code]:", data['zip'])
-        print ("By "+yellow)
+import requests
+me=input("enter ip: ")
+response = requests.get("https://ipgeolocation.abstractapi.com/v1/?api_key=5df4ea3f9fbc4850b6fbc3fcdb19cf04&ip_address=",me)
+print(response.status_code)
+print(response.ip_address)
+print(response.city)
+print(response.city_geoname_id)
