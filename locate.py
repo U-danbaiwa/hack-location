822e148c21dd495be59393c2dfc17500c17f29bf
DANBAIWA HACKER <78322454+U-danbaiwa@users.noreply.github.com>
2021-02-01 12:27:23 +0100

    Update locate.py

--- locate.py
+++ locate.py
@@ -5,12 +5,12 @@ import json
 ip_address =input("Enter ip")
 
 # URL to send the request to
-request_url = 'https://geolocation-db.com/jsonp/' + ip_address
+request_url = 'https://www.ip2location.com/demo + ip_address
 # Send request and decode the result
 response = requests.get(request_url)
 result = response.content.decode()
 # Clean the returned string so it just contains the dictionary data for the IP address
-result = result.split("(")[1].strip(")")
+#result = result.split("(")[1].strip(")")
 # Convert this data into a dictionary
 result  = json.loads(result)
 print(result)
