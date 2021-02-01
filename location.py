2da5d71d3606fdff5a673117a5d92f38a53f9f1b
DANBAIWA HACKER <78322454+U-danbaiwa@users.noreply.github.com>
2021-02-01 17:19:21 +0100

    Update location.py

--- location.py
+++ location.py
@@ -1,6 +1,5 @@
 import requests
-ip=input("enter ip: ")
-response = requests.get("https://ipgeolocation.abstractapi.com/v1/?api_key=5df4ea3f9fbc4850b6fbc3fcdb19cf04&ip_adress=",ip)
-print("victim: ",response['ip'])
-print("City: ",response['city'])
-print(response.content)
+
+response = requests.get("https://ipgeolocation.abstractapi.com/v1/?api_key=5df4ea3f9fbc4850b6fbc3fcdb19cf04&ip_address=197.210.174.187")
+print(response.status_code)
+#print(response.content)
