f665236892c7abae01bab11369d8ecf982fa450c
DANBAIWA HACKER <78322454+U-danbaiwa@users.noreply.github.com>
2021-02-23 11:17:15 +0100

    update U-location.py

--- U-location.py
+++ U-location.py
@@ -1,8 +1,9 @@
+
 import requests, json
 import os
 import sys
 import time 
-#Colours that i will use
+#oColours that i will use
 red = '\033[31m'
 yellow = '\033[93m'
 green = '\033[92m'
@@ -28,9 +29,10 @@ for i in range(90):
 		print(cya+"\t\t   coded By U-danbaiwa")
 		print("")
 		print("")
-		ip=input(green+bold+"[*] Enter Victim Ip:  ")
 		response ="http://ip-api.com/json/"
 		re=requests.get(response+ip).json()
+		ip=input(green+bold+"[*] Enter Victim Ip:  ")
+		
 		print("")
 		print("")
 		print(yellow+bold+"**********************VICTIM INFORMATION*******************")
@@ -89,7 +91,7 @@ for i in range(90):
 	       print("please wait...")
 	       time.sleep(5)
 	       os.system("cd /data/data/com.termux/files/home")
-	       os.system("ls /data/data/com.termux/files/home && rm -rf hack-location")
+	       os.system("ls && rm -rf hack-location")
 	       os.system("pkg update -y")
 	       os.system("pkg install -y git")
 	       os.system("pkg install python")
